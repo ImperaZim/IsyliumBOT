4ee0a0584f68ff6ef7cd967276af3524e093e2c9
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-03 01:01:08 -0300

    update src/handlers/commands/mapper/🌐 Global/ping.ts

--- src/handlers/commands/mapper/🌐 Global/ping.ts
+++ src/handlers/commands/mapper/🌐 Global/ping.ts
@@ -1,6 +1,7 @@
 import { client } from "@main";
 import { CommandProps } from "@types";
 import { ExtendedCommand } from "@extensions";
+import { TranscriptGenerator } from "@handlers/transcripts";
 import { ApplicationCommandType } from "discord.js";
 
 export default new ExtendedCommand({
@@ -8,6 +9,40 @@ export default new ExtendedCommand({
   description: "Verificar o ping do isylium",
   type: ApplicationCommandType.ChatInput,
   async run({ interaction }: CommandProps) {
-    interaction.reply(`Pong! ${client.ws.ping}ms.`);
+  
+  
+ const channel = interaction.channel
+ 
+ if (!channel || channel.type !== 'GUILD_TEXT') {
+        return interaction.reply({
+          content: 'Este comando não pode ser usado em canais privados.',
+          ephemeral: true,
+        });
+      }
+const transcriptGenerator = new TranscriptGenerator(channel, {
+        limit: -1, // Opção para obter todas as mensagens
+        filename: 'transcript.html', // Nome do arquivo
+        saveImages: true, // Inclui as imagens no arquivo HTML
+      });
+
+      try {
+        // Gera a transcrição
+        const transcript = await transcriptGenerator.generateTranscript();
+/*        await message.channel.send({  content: "Aqui está a transcrição:",
+          files: [transcript.attachment],
+        });*/
+
+        // Faz o upload da transcrição
+        const response = await transcriptGenerator.uploadTranscript();
+        if (response) {
+          console.log(`Transcrição enviada com sucesso! Resposta do servidor: ${response}`);
+        } else {
+          console.loh('Houve um erro ao enviar a transcrição.');
+        }
+     } catch (error) {
+        console.error('Erro ao gerar ou enviar transcrição:', error);
+        console.log('Erro ao gerar ou enviar a transcrição.');
+      }
+ //   interaction.reply(`Pong! ${client.ws.ping}ms.`);
   }
 });
