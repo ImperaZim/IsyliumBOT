621865ee53fee6a17be6a441ad581c5661d9cd61
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-02 23:25:35 -0300

    update src/types/definitions/index.ts

--- src/types/definitions/index.ts
+++ src/types/definitions/index.ts
@@ -2,3 +2,4 @@ export * from "./EventInterface";
 export * from "./ConfigInterface";
 export * from "./CommandInterface";
 export * from "./TranscriptInterface";
+export * from "./GuildDataInterface";
