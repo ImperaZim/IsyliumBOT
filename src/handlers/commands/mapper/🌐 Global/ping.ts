672070eff299d026340d620ffb5c0a3a4a61dd27
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-09 01:48:50 -0300

    update src/handlers/commands/mapper/🌐 Global/ping.ts

--- src/handlers/commands/mapper/🌐 Global/ping.ts
+++ src/handlers/commands/mapper/🌐 Global/ping.ts
@@ -13,7 +13,9 @@ export default new ExtendedCommand({
   async run({ interaction }: CommandProps) {
     const dash = "https://dash.isylium.cloud"
 //const application = new Application('HOST', 'TOKEN'); // for application API
-const client = new Client(dash, 'ptlc_XKtjwcPeZ6VvzL35D4WTUVyJwnRGzOAXWoGe3quVGPR');
-client.sendCommand('67471a7a', 'player 1').then((res) => console.log(res)) // res = Successfuly sent the command!
+// const client = new Client(dash, 'ptlc_XKtjwcPeZ6VvzL35D4WTUVyJwnRGzOAXWoGe3quVGPR');
+// client.sendCommand('67471a7a', 'player 1').then((res) => console.log(res)) // res = Successfuly sent the command!
+
+await TranscriptGenerator.fetchAndCheckPlayerData("imperazim", "VdOND9")
   }
 });
