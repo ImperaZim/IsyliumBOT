45938b342f3e1f9f0909afb6416a98138999396b
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-10 03:14:45 -0300

    update src/handlers/discord/mapper/connection.ts

--- src/handlers/discord/mapper/connection.ts
+++ src/handlers/discord/mapper/connection.ts
@@ -46,7 +46,7 @@ export class Connection {
         const response = await axios.get(url);
         interaction.reply({ content: '<:corrector:1293816417984184352> Sua conta foi vinculada ao servidor ${server} com sucesso!.', ephemeral: true });
       } else {
-        interaction.reply({ content: '<:corrector:1293816417984184352> Sua conta foi não foi vinculada ao servidor ${server} ja existe cadastro!.', ephemeral: true });
+        interaction.reply({ content: '<:error:1293818735114846220> Sua conta foi não foi vinculada ao servidor ${server} ja existe cadastro!.', ephemeral: true });
       }
     } catch (error) {
       console.error('Erro ao buscar ou verificar os dados:', (error as Error).message);
