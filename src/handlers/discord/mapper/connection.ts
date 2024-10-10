c2afc4617d2ac1e5a347538a99f0c14cc849430d
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-10 03:24:35 -0300

    update src/handlers/discord/mapper/connection.ts

--- src/handlers/discord/mapper/connection.ts
+++ src/handlers/discord/mapper/connection.ts
@@ -35,8 +35,8 @@ export class Connection {
       const decodedMetadata: DocodedMetadata = JSON.parse(atob(data.metadata));
 
       if (decodedMetadata.discord_link_status) {
-      return interaction.reply({ content: `<:error:1293818735114846220> Sua conta foi não foi vinculada ao servidor ${server} ja existe cadastro!.`, ephemeral: true });
-        
+        return interaction.reply({ content: `<:error:1293818735114846220> Sua conta ja evinculada ao servidor ${server} ja existe cadastro!.`, ephemeral: true });
+
       }
 
       const tokenMatches = decodedMetadata.discord_token === expectedToken;
@@ -47,7 +47,7 @@ export class Connection {
         const response = await axios.get(url);
         interaction.reply({ content: `<:corrector:1293816417984184352> Sua conta foi vinculada ao servidor ${server} com sucesso!.`, ephemeral: true });
       } else {
-        interaction.reply({ content: `<:error:1293818735114846220> Verifique token ou username e tebte novamente!.`, ephemeral: true });
+        interaction.reply({ content: `<:error:1293818735114846220> Verifique token ou username e tente novamente!.`, ephemeral: true });
       }
     } catch (error) {
       console.error('Erro ao buscar ou verificar os dados:', (error as Error).message);
