adb9d565d8f57ad297dac5ac19b93b9a6681d67e
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-11 19:27:54 -0300

    update src/handlers/commands/mapper/🌐 Global/ping.ts

--- src/handlers/commands/mapper/🌐 Global/ping.ts
+++ src/handlers/commands/mapper/🌐 Global/ping.ts
@@ -3,7 +3,7 @@ import { CommandProps } from "@types";
 import { ExtendedCommand } from "@extensions";
 import { TranscriptGenerator } from "@handlers/transcripts";
 import { Connection } from "@handlers/discord"
-import { MercadoPagoConfig, PaymentMethod, Preference } from 'mercadopago';
+import { MercadoPagoConfig, PaymentMethod } from 'mercadopago';
 import { ApplicationCommandType } from "discord.js";
 import { Client, Application } from 'jspteroapi';
 
@@ -12,9 +12,15 @@ export default new ExtendedCommand({
   description: "Verificar o ping do isylium",
   type: ApplicationCommandType.ChatInput,
   async run({ interaction }: CommandProps) {
-    const dash = "https://dash.isylium.cloud"
+  //  const dash = "https://dash.isylium.cloud"
     //const application = new Application('HOST', 'TOKEN'); // for application API
     // const client = new Client(dash, 'ptlc_XKtjwcPeZ6VvzL35D4WTUVyJwnRGzOAXWoGe3quVGPR');
     // client.sendCommand('67471a7a', 'player 1').then((res) => console.log(res)) // res = Successfuly sent the command!
+    
+    const client = new MercadoPagoConfig({ accessToken: 'APP_USR-6462663530067323-100302-1eb4b8ec5ae36bd96ec504f6d708b90b-779023770' });
+      const paymentMethods = new PaymentMethod(client);
+    
+         paymentMethods.get().then((result) => console.log(result))
+        .catch((error) => console.log(error));
   }
 });
