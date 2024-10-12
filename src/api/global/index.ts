ef0f006724a109d1a29fdd0c50eb5ddc47bc4924
impera <imperaartz1@gmail.com>
2024-10-12 20:53:28 -0300

    ...

--- src/api/global/index.ts
+++ src/api/global/index.ts
@@ -10,37 +10,17 @@ export function startExpress() {
     res.send('Hello World')
   })
 
-  app.get('/harvest/getplayerdata/:playerName?', (req, res) => {
-    const playerName = req.params.playerName;
+  app.post('/notification/receive', (req, res) => {
+    const json = req.body;
 
-    // Verifica se o nome do jogador foi passado
-    if (!playerName) {
-      return res.status(400).json({ error: 'Nome do jogador não foi fornecido' });
+    if (!json) {
+      return res.status(400).json({ error: 'Json não foi fornecido' });
     }
 
-    const playerFile = `${playerName}.json`;
-    const filePath = path.join(directoryPath, playerFile);
-
-    // Verifica se o arquivo do jogador existe
-    if (!fs.existsSync(filePath)) {
-      return res.status(404).json({ error: 'Jogador não encontrado' });
-    }
-
-    // Lê o conteúdo do arquivo JSON do jogador
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        return res.status(500).json({ error: 'Erro ao ler o arquivo do jogador' });
-      }
-
-      try {
-        const playerData = JSON.parse(data);
-        res.json(playerData);
-      } catch (err) {
-        console.error(`Erro ao analisar o JSON do jogador: ${playerName}`);
-        res.status(500).json({ error: 'Erro ao analisar os dados do jogador' });
-      }
-    });
+    res.status(200).json({ message: 'Notificação recebida com sucesso', data: json });
+    console.log(JSON.parse(json));
   });
+
 
   app.listen(19134)
 }
