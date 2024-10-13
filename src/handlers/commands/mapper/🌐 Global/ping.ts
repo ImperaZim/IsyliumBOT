c873f89b4360a58fcc42424fcdc0656bdad020ba
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-12 21:52:57 -0300

    update src/handlers/commands/mapper/🌐 Global/ping.ts

--- src/handlers/commands/mapper/🌐 Global/ping.ts
+++ src/handlers/commands/mapper/🌐 Global/ping.ts
@@ -3,7 +3,7 @@ import { CommandProps } from "@types";
 import { ExtendedCommand } from "@extensions";
 import { TranscriptGenerator } from "@handlers/transcripts";
 import { Payments } from "@handlers/mercadopago"
-import mercadopago, { MercadoPagoConfig } from 'mercadopago';
+import mercadopago, { MercadoPagoConfig, Preference } from 'mercadopago';
 import { ApplicationCommandType } from "discord.js";
 import { Client, Application } from 'jspteroapi';
 
@@ -20,14 +20,64 @@ export default new ExtendedCommand({
 
 
 
+
+
+
+    const body = {
+      items: [
+        {
+          title: 'My product',
+          quantity: 1,
+          unit_price: 2000
+        }
+      ],
+      payer: {
+        name: 'Test',
+        surname: 'User',
+        email: 'your_test_email@example.com',
+        phone: {
+          area_code: '11',
+          number: '4444-4444',
+        },
+        address: {
+          zip_code: '06233200',
+          street_name: 'Street',
+          street_number: 123,
+        },
+      },
+      payment_methods: {
+        excluded_payment_methods: [
+          {
+            id: "amex"
+          },
+          {
+            id: "elo"
+          },
+          {
+            id: "hipercard"
+          },
+          {
+            id: "bolbradesco"
+          },
+          {
+            id: "pec"
+          }
+        ],
+        excluded_payment_types: [],
+        installments: 5
+      }
+    };
+
+
+
     // Configurar Mercado Pago com o token de acesso
     const client = new MercadoPagoConfig({ accessToken: 'APP_USR-6462663530067323-100302-1eb4b8ec5ae36bd96ec504f6d708b90b-779023770' });
 
-    
-    
+    const preference = new Preference(client);
+
 
     // Criar uma preferência
-    const preference = {
+    const preferences = {
       items: [
         {
           title: 'Meu Produto',
@@ -38,10 +88,9 @@ export default new ExtendedCommand({
     };
 
     // Enviar a preferência
-    client.preferences.create(preference)
-      .then(response => {
-        console.log(response.body.init_point); // Link gerado para pagamento
-      })
+    await preference.create({ preferences }).then(response => {
+      console.log(response.body.init_point); // Link gerado para pagamento
+    })
       .catch(error => {
         console.error(error);
       });
