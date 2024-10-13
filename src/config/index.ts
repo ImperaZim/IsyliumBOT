0b176400a51b9f293d8eacab4c551a20fa7c78c6
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-12 23:33:34 -0300

    update src/config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,9 @@ export const discord: Config = {
   id: "1261824986885390421",
   token: "MTI2MTgyNDk4Njg4NTM5MDQyMQ.GI8k9s.097_qhDTDirViRxR1ElqByKj2O9xHHtPNkPJ68",
 };
+export const mercadopago: config = {
+  acesstoken: "APP_USR-6261249818160055-101222-b7a56ec6ebee67497829816254d9c498-779023770"
+}
 export const color: Config = {
   red: "#f42121",
   gray: "#2d2929",
