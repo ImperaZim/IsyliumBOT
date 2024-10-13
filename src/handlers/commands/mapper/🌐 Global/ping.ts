6fe320923cd82400a286897332ed0a023e08ef21
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-12 21:21:32 -0300

    update src/handlers/commands/mapper/🌐 Global/ping.ts

--- src/handlers/commands/mapper/🌐 Global/ping.ts
+++ src/handlers/commands/mapper/🌐 Global/ping.ts
@@ -12,53 +12,53 @@ export default new ExtendedCommand({
   description: "Verificar o ping do isylium",
   type: ApplicationCommandType.ChatInput,
   async run({ interaction }: CommandProps) {
-  //  const dash = "https://dash.isylium.cloud"
+    //  const dash = "https://dash.isylium.cloud"
     //const application = new Application('HOST', 'TOKEN'); // for application API
     // const client = new Client(dash, 'ptlc_XKtjwcPeZ6VvzL35D4WTUVyJwnRGzOAXWoGe3quVGPR');
     // client.sendCommand('67471a7a', 'player 1').then((res) => console.log(res)) // res = Successfuly sent the command!
-    
 
-        
 
-        const client = new MercadoPagoConfig({ accessToken: 'YOUR_ACCESS_TOKEN' });
 
-        const preference = new Preference(client);
 
-        preference.create({
-          body: {
-            payment_methods: {
+    const client = new MercadoPagoConfig({ accessToken: 'APP_USR-6462663530067323-100302-1eb4b8ec5ae36bd96ec504f6d708b90b-779023770' });
+
+    const preference = new Preference(client);
+
+    preference.create({
+      body: {
+        payment_methods: {
           excluded_payment_methods: [
-                    {
-                              id: "amex"
-                    },
-                    {
-                              id: "elo"
-                    },
-                    {
-                              id: "hipercard"
-                    },
-                    {
-                              id: "bolbradesco"
-                    },
-                    {
-                              id: "pec"
-                    }
+            {
+              id: "amex"
+            },
+            {
+              id: "elo"
+            },
+            {
+              id: "hipercard"
+            },
+            {
+              id: "bolbradesco"
+            },
+            {
+              id: "pec"
+            }
           ],
           excluded_payment_types: [],
           installments: 12
-},
-            items: [
-              {
-                title: 'My product',
-                quantity: 1,
-                unit_price: 2000
-              }
-            ],
+        },
+        items: [
+          {
+            title: 'My product',
+            quantity: 1,
+            unit_price: 2000
           }
-        })
-        .then(console.log)
-        .catch(console.log);
-       
+        ],
+      }
+    })
+      .then(console.log)
+      .catch(console.log);
+
 
   }
 });
