1640aafe07c1c16b5000a19da06e3bbcba46095b
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-13 17:15:43 -0300

    update src/handlers/commands/mapper/🌐 Global/ping.ts

--- src/handlers/commands/mapper/🌐 Global/ping.ts
+++ src/handlers/commands/mapper/🌐 Global/ping.ts
@@ -5,7 +5,7 @@ import { TranscriptGenerator } from "@handlers/transcripts";
 import { Payment } from "@handlers/mercadopago"
 import { ApplicationCommandType } from "discord.js";
 import { Client, Application } from 'jspteroapi';
-import { mercadopago} from "@config";
+import { mercadopago } from "@config";
 import axios from 'axios';
 
 export default new ExtendedCommand({
@@ -13,19 +13,19 @@ export default new ExtendedCommand({
   description: "Verificar o ping do isylium",
   type: ApplicationCommandType.ChatInput,
   async run({ interaction }: CommandProps) {
-    
-const pay = await new Payment(mercadopago.acesstoken);
- try {
- await pay.createPayment(interaction.user, "yBriisMC", "gold", "10000")
-if (pay) {
 
-        await interaction.reply(`Seu link de pagamento: ${pay}`);
+    const pay = await new Payment(mercadopago.acesstoken);
+    try {
+      await pay.createPayment(interaction.user, "yBriisMC", "gold", "10000")
+      if (pay) {
+
+        await interaction.reply({ content: `Seu link de pagamento: ${pay}`});
       } else {
-        await interaction.reply('Houve um erro ao gerar o pagamento.');
+        await interaction.reply({ content: 'Houve um erro ao gerar o pagamento.'});
       }
     } catch (error) {
       console.error(error);
-      await interaction.reply('Ocorreu um erro ao processar o pagamento.');
+      await interaction.reply({ content: 'Ocorreu um erro ao processar o pagamento.'});
     }
 
   }
