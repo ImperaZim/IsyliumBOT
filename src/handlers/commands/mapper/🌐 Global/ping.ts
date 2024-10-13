4dd5703cd2aa7044bdcd1f7d7f53ceaeb2dae7b5
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-13 01:33:53 -0300

    update src/handlers/commands/mapper/🌐 Global/ping.ts

--- src/handlers/commands/mapper/🌐 Global/ping.ts
+++ src/handlers/commands/mapper/🌐 Global/ping.ts
@@ -3,7 +3,7 @@ import { CommandProps } from "@types";
 import { ExtendedCommand } from "@extensions";
 import { TranscriptGenerator } from "@handlers/transcripts";
 import { Payments } from "@handlers/mercadopago"
-import { MercadoPagoConfig, Payment } from 'mercadopago';
+import { MercadoPagoConfig, Payment, Preference } from 'mercadopago';
 import { ApplicationCommandType } from "discord.js";
 import { Client, Application } from 'jspteroapi';
 import { payment } from "@config";
@@ -23,24 +23,49 @@ export default new ExtendedCommand({
     // Step 2: Initialize the client object
     const client = new MercadoPagoConfig({ accessToken: payment.acesstoken, options: { timeout: 5000 } });
 
-    // Step 3: Initialize the API object
-    const payments = new Payment(client);
+    // Step 3: Initialize the API objectconst client = new MercadoPago({  accessToken: config.access_token });
+const preference = new Preference(client);
 
-    // Step 4: Create the request object
-    const body = {
-      transaction_amount: 12.34,
-      description: 'a',
-      payment_method_id: 'master',
-      token: "58",
-      payer: {
-        email: 'ybriismc@gmail.com'
+const body = {
+  items: [
+    {
+      id: '1234',
+      title: 'Dummy Title',
+      description: 'Dummy description',
+      picture_url: 'http://www.myapp.com/myimage.jpg',
+      quantity: 1,
+      currency_id: 'BRL',
+      unit_price: 10,
+    },
+  ],
+  marketplace_fee: 0,
+  payer: {
+    name: 'Test',
+    surname: 'User',
+    email: 'your_test_email@example.com',
+  },
+  additional_info: 'Discount: 12.00',
+  auto_return: 'all',
+  binary_mode: true,
+  external_reference: '1643827245',
+  marketplace: 'marketplace',
+  operation_type: 'regular_payment',
+  payment_methods: {
+    default_payment_method_id: 'master',
+    excluded_payment_types: [
+      {
+        id: 'ticket',
       },
-    };
+    ],
+    installments: 5,
+    default_installments: 1,
+  },
+  statement_descriptor: 'Test Store',
+};
 
-    // Step 5: Create request options object - Optional
+const response = await preference.create({ body })
+  .then(console.log).catch(console.log);
 
-
-    // Step 6: Make the request
-    payments.create({ body }).then(console.log).catch(console.log);
+    
   }
 });
