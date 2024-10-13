9d138c9a7dd3a0fb1a34c441167019c407bf391f
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-13 01:53:31 -0300

    update src/handlers/commands/mapper/🌐 Global/ping.ts

--- src/handlers/commands/mapper/🌐 Global/ping.ts
+++ src/handlers/commands/mapper/🌐 Global/ping.ts
@@ -16,51 +16,51 @@ export default new ExtendedCommand({
   async run({ interaction }: CommandProps) {
 
 
-var axios = require('axios');
-var data = JSON.stringify({
-  "statement_descriptor": "TestStore",
-  "external_reference": "IWD1238971",
-  "items": [
-    {
-      "id": "010983098",
-      "title": "My Product",
-      "quantity": 1,
-      "unit_price": 2000,
-      "description": "Description of my product",
-      "category_id": "retail"
-    }
-  ],
-  "payer": {
-    "email": "test_user_12398378192@testuser.com",
-    "name": "Juan",
-    "surname": "Lopez"
-  },
-  "payment_methods": {
-    "excluded_payment_types": [],
-    "excluded_payment_methods": [],
-    "installments": 12,
-    "default_payment_method_id": "account_money"
-  }
-});
+    var axios = require('axios');
+    var data = JSON.stringify({
+      "statement_descriptor": "TestStore",
+      "external_reference": "IWD1238971",
+      "items": [
+        {
+          "id": "010983098",
+          "title": "My Product",
+          "quantity": 1,
+          "unit_price": 2000,
+          "description": "Description of my product",
+          "category_id": "retail"
+        }
+      ],
+      "payer": {
+        "email": "test_user_12398378192@testuser.com",
+        "name": "Juan",
+        "surname": "Lopez"
+      },
+      "payment_methods": {
+        "excluded_payment_types": [],
+        "excluded_payment_methods": [],
+        "installments": 12,
+        "default_payment_method_id": "pix"
+      }
+    });
 
-var config = {
-  method: 'post',
-maxBodyLength: Infinity,
-  url: 'https://api.mercadopago.com/checkout/preferences',
-  headers: { 
-    'Content-Type': 'application/json',
-    
-  },
-  data : data
-};
+    var config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://api.mercadopago.com/checkout/preferences',
+      headers: {
+        'Content-Type': 'application/json',
+       Authorization: `Bearer ${payment.acesstoken}`
+      },
+      data: data
+    };
 
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
 
   }
