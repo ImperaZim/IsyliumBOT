ebf706c2f4e5e9475902215298aad937013eca0f
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-13 19:38:26 -0300

    update src/handlers/mercadopago/mapper/payment.ts

--- src/handlers/mercadopago/mapper/payment.ts
+++ src/handlers/mercadopago/mapper/payment.ts
@@ -48,8 +48,7 @@ export class Payment {
 
     try {
       const response = await axios(config);
-      console.log(response)
- //     return response.data.init_point;
+     return response.request.data.init_point;
     } catch (error) {
       console.error(error);
       return null;
