2a0653b591b09379499bb012754c2fa19732862a
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-13 17:13:12 -0300

    update src/handlers/commands/mapper/🌐 Global/ping.ts

--- src/handlers/commands/mapper/🌐 Global/ping.ts
+++ src/handlers/commands/mapper/🌐 Global/ping.ts
@@ -2,11 +2,10 @@ import { client } from "@main";
 import { CommandProps } from "@types";
 import { ExtendedCommand } from "@extensions";
 import { TranscriptGenerator } from "@handlers/transcripts";
-//import { Payments } from "@handlers/mercadopago"
-import { MercadoPagoConfig, Payment, Preference } from 'mercadopago';
+import { Payment } from "@handlers/mercadopago"
 import { ApplicationCommandType } from "discord.js";
 import { Client, Application } from 'jspteroapi';
-import { payment } from "@config";
+import { mercadopago} from "@config";
 import axios from 'axios';
 
 export default new ExtendedCommand({
@@ -14,47 +13,9 @@ export default new ExtendedCommand({
   description: "Verificar o ping do isylium",
   type: ApplicationCommandType.ChatInput,
   async run({ interaction }: CommandProps) {
-
-    var data = JSON.stringify({
-      "statement_descriptor": "IsyliumStore",
-      "external_reference": "userid",
-      "items": [
-        {
-          "id": "id",
-          "title": "My Product",
-          "unit_price": 0.1,
-          "category_id": "categoriaid"
-        }
-      ],
-      "payer": {
-        "name": "nickname, username",
-        "surname": "gold, 7000"
-      },
-      "payment_methods": {
-        "excluded_payment_types": [],
-        "excluded_payment_methods": [],
-        "installments": 12
-      }
-    });
-
-    var config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'https://api.mercadopago.com/checkout/preferences',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${payment.acesstoken}`
-      },
-      data: data
-    };
-
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    
+const pay = await new Payment(mercadopago.acesstoken);
+ await pay.createPayment(interaction.user, "yBriisMC", "gold", "10000")
 
 
   }
