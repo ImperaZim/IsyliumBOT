76e0ab53b983344af373a90a5b98a2472df9235a
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-14 20:05:10 -0300

    update src/handlers/commands/mapper/🔐 Moderation/manege.ts

--- src/handlers/commands/mapper/🔐 Moderation/manege.ts
+++ src/handlers/commands/mapper/🔐 Moderation/manege.ts
@@ -3,20 +3,17 @@ import { CommandProps } from "@types";
 import { ExtendedCommand } from "@extensions";
 
 export default new ExtendedCommand({
-  name: "manage",
-  description: "Gerenciar sistemas do servidor",
+  name: "settings",
+  description: "Server systems configuration",
   defaultMemberPermissions: "Administrator",
   type: ApplicationCommandType.ChatInput,
   async run({ interaction }: CommandProps) {
-      if (client.user === null) return;
-      if (!interaction.inCachedGuild()) return;
-      const data = new GuildData(interaction.guild);
-      const checkGuild = await data.checkGuildData();
-      const username = client.user ? client.user.username : "";
-      const displayAvatar = client.user
-        ? client.user.displayAvatarURL() : "";
-
-
+    if (client.user === null) return;
+    if (!interaction.inCachedGuild()) return;
+    const username = client.user ? client.user.username : "";
+    const displayAvatar = client.user
+      ? client.user.displayAvatarURL() : "";
+      
 
   }
 });
