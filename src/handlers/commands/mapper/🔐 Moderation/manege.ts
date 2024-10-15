a14f8ee56c4900ac41d44cd0e7033a14374c0ad4
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-15 04:00:39 -0300

    update src/handlers/commands/mapper/🔐 Moderation/manege.ts

--- src/handlers/commands/mapper/🔐 Moderation/manege.ts
+++ src/handlers/commands/mapper/🔐 Moderation/manege.ts
@@ -56,13 +56,16 @@ export default new ExtendedCommand({
         });
       }
     },
-    "dcl_select_logs",
+]
+  ]),
+  channelselect: new Collection([
+    [
+          "dcl_select_logs",
     async (interaction: any) => {
       console.log(interaction)
     }
-
-  ]
-  ]),
+      ]
+      ])
   buttons: new Collection([[
     "dcl_logs",
     async (interaction: any) => {
