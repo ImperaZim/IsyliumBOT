fdeea709c7d120a6072cc8f7310be5376b9da285
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-14 21:06:56 -0300

    update src/handlers/client/mapper/Create.ts

--- src/handlers/client/mapper/Create.ts
+++ src/handlers/client/mapper/Create.ts
@@ -1,5 +1,6 @@
 import { Guild } from "discord.js";
 import { mysql } from "@main";
+import { RowDataPacket } from "@types";
 
 export class CreatedGuild {
   private guild: Guild;
@@ -8,17 +9,30 @@ export class CreatedGuild {
     this.guild = guild;
   }
 
-public async checkAndAddGuild(): Promise<boolean> {
-    const guildId = this.guild.id;
+  private async addGuild(): Promise<boolean> {
+    const guildId: string = this.guild.id;
 
     try {
-      const DiscordLink = await mysql.select(
+      await mysql.insert("discord_link", { guildid: guildId, created_at: new Date() });
+      await mysql.insert("ticket_discord", { guildid: guildId, created_at: new Date() });
+      return true;
+    } catch (error) {
+      console.error(`Erro ao adicionar guilda ${guildId}:`, error);
+      return false;
+    }
+  }
+
+  public async checkAndAddGuild(): Promise<boolean> {
+    const guildId: string = this.guild.id;
+
+    try {
+      const DiscordLink: RowDataPacket[] | null = await mysql.select(
         "discord_link",
         "guildid",
         [{ guildid: guildId }]
       );
 
-      const TicketDiscord = await mysql.select(
+      const TicketDiscord: RowDataPacket[] | null = await mysql.select(
         "ticket_discord",
         "guildid",
         [{ guildid: guildId }]
@@ -32,19 +46,6 @@ public async checkAndAddGuild(): Promise<boolean> {
       return false;
     } catch (error) {
       console.error(`Erro ao verificar ou adicionar guilda ${guildId}:`, error);
-      return false;
-    }
-  }
-
-  private async addGuild(): Promise<boolean> {
-    const guildId = this.guild.id;
-
-    try {
-      await mysql.insert("discord_link", { guildid: guildId, created_at: new Date() });
-      await mysql.insert("ticket_discord", { guildid: guildId, created_at: new Date() });
-      return true;
-    } catch (error) {
-      console.error(`Erro ao adicionar guilda ${guildId}:`, error);
       return false;
     }
   }
