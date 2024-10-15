9b1a780d5f64f469a902526a4180682b824daa35
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-14 23:21:27 -0300

    update src/handlers/elementors/settings/mapper/index.ts

--- src/handlers/elementors/settings/mapper/index.ts
+++ src/handlers/elementors/settings/mapper/index.ts
@@ -11,7 +11,7 @@ export function register(): void {
         color: color.primary,
         description:
           "> Olá {user}, logo abaixo está todos os meus sistemas disponíveis para configuração",
-        thumbnail: isylium,
+        thumbnail: profile.icon,
         image: profile.icon,
         author: {
           name: `Isylium`,
