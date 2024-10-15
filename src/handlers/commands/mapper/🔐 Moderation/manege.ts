7713a527b5ea76f32edc62df007e38594b5761f5
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-15 17:55:23 -0300

    update src/handlers/commands/mapper/🔐 Moderation/manege.ts

--- src/handlers/commands/mapper/🔐 Moderation/manege.ts
+++ src/handlers/commands/mapper/🔐 Moderation/manege.ts
@@ -107,13 +107,12 @@ export default new ExtendedCommand({
           new ActionRowBuilder<ChannelSelectMenuBuilder>().addComponents([select_logs])
         ]
       });
-    }
-  ],
-      [
+    },
     "dcl_embed",
     async (interaction: any) => {
       const user = interaction.user;
       const modal_embed = getModal("dcl_embed_modal");
+      await interaction.showModal(modal_embed);
     }
     ]
   ])
