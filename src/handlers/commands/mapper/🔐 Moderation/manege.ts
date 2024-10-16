5c9408a9b1d7521aa268083aeb632e1e6c48a471
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-16 17:12:23 -0300

    update src/handlers/commands/mapper/🔐 Moderation/manege.ts

--- src/handlers/commands/mapper/🔐 Moderation/manege.ts
+++ src/handlers/commands/mapper/🔐 Moderation/manege.ts
@@ -113,6 +113,7 @@ export default new ExtendedCommand({
       "dcl_embed",
       async (interaction: any) => {
         const user = interaction.user;
+        await interaction.deferUpdate();
         const modal_embed = getModal("dcl_embed_modal");
         await interaction.showModal(modal_embed);
       }
