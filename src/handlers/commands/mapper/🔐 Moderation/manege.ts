fad79a3db938f2e2747b7f4616fedc3628e0a89f
Impera <imperaartz1@gmail.com>
2024-10-17 02:06:00 -0300

    update src/handlers/commands/mapper/🔐 Moderation/manege.ts

--- src/handlers/commands/mapper/🔐 Moderation/manege.ts
+++ src/handlers/commands/mapper/🔐 Moderation/manege.ts
@@ -3,35 +3,39 @@ import { CommandProps } from "@types";
 import { ExtendedCommand } from "@extensions";
 import { CreatedGuild, SettingsController } from "@handlers";
 import { getSelect, getEmbed } from "DiscordElementor";
-import { ApplicationCommandType, ActionRowBuilder } from "discord.js";
+import { ApplicationCommandType, ActionRowBuilder, StringSelectMenuBuilder } from "discord.js";
+
 const settings = "settings";
 
 export default new ExtendedCommand({
-    name: "settings",
-    description: "Server systems configuration",
-    defaultMemberPermissions: "Administrator",
-    type: ApplicationCommandType.ChatInput,
-    async run({ interaction }: CommandProps) {
-        if (!interaction.inCachedGuild()) return;
-        const { user, guild } = interaction;
-        const data = new CreatedGuild(guild).checkAndAddGuild();
-        if (data) {
-            const embed_start = getEmbed(settings, "embed_settings", {
-                user: user.globalName || "error 404"
-            });
-            const select_start = getSelect(settings, "select_settings");
+  name: "settings",
+  description: "Server systems configuration",
+  defaultMemberPermissions: "Administrator",
+  type: ApplicationCommandType.ChatInput,
+  async run({ interaction }: CommandProps) {
+    if (!interaction.inCachedGuild()) return;
 
-            const response = await interaction.reply({
-                embeds: [embed_start],
-                components: [
-                    new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
-                        [select_start]
-                    )
-                ],
-                fetchReply: true
-            });
-            const controller = new SettingsController(response, user, guild);
-            controller.startCollectors();
-        }
+    const { user, guild } = interaction;
+    const data = new CreatedGuild(guild).checkAndAddGuild();
+
+    if (data) {
+      const embed_start = getEmbed(settings, "embed_settings", {
+        user: user.globalName || "error 404"
+      });
+      const select_start = getSelect(settings, "select_settings");
+
+      const response = await interaction.reply({
+        embeds: [embed_start],
+        components: [
+          new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+            [select_start]
+          )
+        ],
+        fetchReply: true
+      });
+      
+      const controller = new SettingsController(user, guild);
+      await controller.startSettingsInteraction(response);
     }
+  }
 });
