9d141a15ac15a305303e8640e08aa2bd88322a1b
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-16 23:27:29 -0300

    update src/handlers/elementors/settings/mapper/collectors/index.ts

--- src/handlers/elementors/settings/mapper/collectors/index.ts
+++ src/handlers/elementors/settings/mapper/collectors/index.ts
@@ -16,9 +16,23 @@ import {
 import { client, mysql } from '@main';
   
   export function Responder(response: any, user: User, guild: Guild){
-    new SelectColletor(response, 
+   return new SelectColletor(response, 
     (select: SelectMenuInteraction ) => {
-      
+      const { values, user } = select
+      const options = values[0];
+        if (options === "settings:discordlink") {
+          const embed_discordlink = getEmbed(settings, "settings_discordlink", {
+            user: user.globalName || "error 404",
+          });
+          const buttons = ["dcl_embed", "dcl_logs", "dcl_servers"].map((buttonName) => getButton(settings, buttonName));
+          const row = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons)
+          const components = ([row] || []).map((ar) => ar.toJSON());
+
+           select.reply({
+            embeds: [embed_discordlink],
+            components: components
+          });
+        }
     },
     ComponentType.StringSelect, 
       60000, 
