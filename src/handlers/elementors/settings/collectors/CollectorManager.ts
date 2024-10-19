b30350e6002077d7798f719044da054f081597e0
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-19 00:40:03 -0300

    update src/handlers/elementors/settings/collectors/CollectorManager.ts

--- src/handlers/elementors/settings/collectors/CollectorManager.ts
+++ src/handlers/elementors/settings/collectors/CollectorManager.ts
@@ -77,9 +77,11 @@ export class CollectorsManager {
                 switch (select.customId) {
                     case "discord_logs_select":
                         console.log(selected);
-                        
-                        
-
+mysql.update(
+          "discord_link",
+          { logs: selected },
+          [{ guildid: this.guild.id }]
+        );
                         PageManager.loadPage("open:discord_link_settings", {
                             interaction,
                             collectorResponse: select
