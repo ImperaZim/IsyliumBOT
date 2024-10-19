a2257c96068e5bbf8af7ce3ed4ce874be3932d62
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-19 00:53:00 -0300

    update src/handlers/elementors/settings/functions/PageManager.ts

--- src/handlers/elementors/settings/functions/PageManager.ts
+++ src/handlers/elementors/settings/functions/PageManager.ts
@@ -3,66 +3,82 @@ import { ActionRowBuilder } from "discord.js";
 import { LoadPagesinterface } from "@types";
 
 export class PageManager {
-  private static settings = "settings";
+    private static settings = "settings";
 
-  static async loadPage(id: string, properties: LoadPagesinterface) {
-    let embeds = [];
-    let components = [];
-    const { user } = properties.interaction;
+    static async loadPage(id: string, properties: LoadPagesinterface) {
+        let embeds = [];
+        let components = [];
+        const { user } = properties.interaction;
 
-    switch (id) {
-      case "open:settings_main_menu":
-        embeds.push(
-          getEmbed(this.settings, "settings_main_menu", {
-            user: user.globalName || "error 404"
-          })
-        );
-        components.push(getSelect(this.settings, "settings_menu"));
-        break;
+        switch (id) {
+            case "open:settings_main_menu":
+                embeds.push(
+                    getEmbed(this.settings, "settings_main_menu", {
+                        user: user.globalName || "error 404"
+                    })
+                );
+                components.push(getSelect(this.settings, "settings_menu"));
+                break;
 
-      case "open:discord_logs_select":
-        embeds.push(
-          getEmbed(this.settings, "discord_logs_setup", {
-            user: user.globalName || "error 404"
-          })
-        );
-        components.push(
-          getSelect(this.settings, "discord_logs_select")
-        );
-        break;
+            case "open:discord_logs_select":
+                embeds.push(
+                    getEmbed(this.settings, "discord_logs_setup", {
+                        user: user.globalName || "error 404"
+                    })
+                );
+                components.push(
+                    getSelect(this.settings, "discord_logs_select")
+                );
+                break;
+                case "open:discord_server_manager":
+                  embeds.push(
+                    getEmbed(this.settings, "discord_servers_setup", {
+                        user: user.globalName || "error 404"
+                    })
+                );components.push(
+                    getButton(this.settings, "discord_server_delete")
+                );
+                components.push(
+                    getButton(this.settings, "discord_server_edit")
+                );
+                components.push(
+                    getButton(this.settings, "discord_server_create")
+                );
+                
+                break;
 
-      case "open:discord_link_settings":
-        embeds.push(
-          getEmbed(this.settings, "discord_link_settings", {
-            user: user.globalName || "error 404"
-          })
-        );
-        components.push(
-          getButton(this.settings, "discord_embed_creator")
-        );
-        components.push(
-          getButton(this.settings, "discord_log_channel")
-        );
-        components.push(
-          getButton(this.settings, "discord_server_manager")
-        );
-        break;
-      default:
-        break;
+            case "open:discord_link_settings":
+                embeds.push(
+                    getEmbed(this.settings, "discord_link_settings", {
+                        user: user.globalName || "error 404"
+                    })
+                );
+                components.push(
+                    getButton(this.settings, "discord_embed_creator")
+                );
+                components.push(
+                    getButton(this.settings, "discord_log_channel")
+                );
+                components.push(
+                    getButton(this.settings, "discord_server_manager")
+                );
+                break;
+            default:
+                break;
+        }
+
+        components = [new ActionRowBuilder().addComponents(...components)];
+
+        if (properties.collectorResponse) {
+            properties.collectorResponse.update({
+                embeds: embeds,
+                components: components
+            });
+        } else if (properties.message) {
+            properties.message.edit({
+                embeds: embeds,
+                components: components
+            });
+        }
     }
-
-    components = [new ActionRowBuilder().addComponents(...components)];
-
-    if (properties.collectorResponse) {
-      properties.collectorResponse.update({
-        embeds: embeds,
-        components: components
-      });
-    } else if (properties.message) {
-      properties.message.edit({
-        embeds: embeds,
-        components: components
-      });
-    }
-  }
 }
