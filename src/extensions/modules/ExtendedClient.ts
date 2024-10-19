1a1792d50a2fc196239d5df20ce4a3dea8e8b4f2
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-19 14:28:20 -0300

    update src/extensions/modules/ExtendedClient.ts

--- src/extensions/modules/ExtendedClient.ts
+++ src/extensions/modules/ExtendedClient.ts
@@ -110,7 +110,7 @@ export class ExtendedClient extends Client {
     ]);
      mysql.createTable('ticket_discord', [
       { name: 'guildid', type: 'VARCHAR(255) NOT NULL PRIMARY KEY' },
-      { name: 'embeds_json', type: 'JSON DEFAULT NULL' },
+      { name: 'embeds_json', type: 'LONGTEXT DEFAULT NULL' },
       { name: 'role_perms', type: 'VARCHAR(255) DEFAULT NULL' },  
       { name: 'logs', type: 'VARCHAR(255) DEFAULT NULL' },        
       { name: 'send_channel', type: 'VARCHAR(255) DEFAULT NULL' },
@@ -119,8 +119,8 @@ export class ExtendedClient extends Client {
     ]);
     mysql.createTable('discord_link', [
       { name: 'guildid', type: 'VARCHAR(255) NOT NULL PRIMARY KEY' },
-      { name: 'servers', type: 'JSON DEFAULT NULL' },
-      { name: 'embeds_json', type: 'JSON DEFAULT NULL' },
+      { name: 'servers', type: 'LONGTEXT DEFAULT NULL' },
+      { name: 'embeds_json', type: 'LONGTEXT DEFAULT NULL' },
       { name: 'logs', type: 'VARCHAR(255) DEFAULT NULL' },
       { name: 'send_channel', type: 'VARCHAR(255) DEFAULT NULL' },
       { name: 'created_at', type: 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP' }
