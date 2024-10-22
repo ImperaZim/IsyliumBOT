cdbd3b7671ce8ea6021a3f2fe5ca4b6bede3a30d
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-22 18:45:43 -0300

    update src/config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -19,4 +19,11 @@ export const color: Config = {
 export const profile: Config = {
   icon: "https://cdn.discordapp.com/attachments/1261777733399347313/1295560563585454100/f1968b752dd03cce586e494eac75c237-1.png?ex=670f184c&is=670dc6cc&hm=c80a6d80e14b455d0fa9d49c63be64e9ea7ad278863a7ce2caf990d0e7d966b5&",
   thumb: "https://cdn.discordapp.com/attachments/1261777733399347313/1295560563585454100/f1968b752dd03cce586e494eac75c237-1.png?ex=670f184c&is=670dc6cc&hm=c80a6d80e14b455d0fa9d49c63be64e9ea7ad278863a7ce2caf990d0e7d966b5&"
-}
+};
+
+export const assets: Config = {
+  ticket: "https://cdn.discordapp.com/attachments/1298374782810587219/1298374815127699477/4961736.png?ex=67195546&is=671803c6&hm=45b45964b3a4708ccf342017f6d793d153f909b9ec9dcc53972fd2dfa890f644&",
+  url: "https://cdn.discordapp.com/attachments/1298374782810587219/1298376388252012605/link.png?ex=671956bd&is=6718053d&hm=f88c913bf3ba1c00ce6a9e6f44ae323fad44410bd3040b0a957143b81f18c6dd&",
+  settings: "https://cdn.discordapp.com/attachments/1298374782810587219/1298376387568336978/settings.png?ex=671956bd&is=6718053d&hm=ddb4f93e6a73672f17c4f458e7cc287e8799dfb07c0bffbfcb70cd20574f76c2&",
+  cart: "https://cdn.discordapp.com/attachments/1298374782810587219/1298375629636501545/carrinho-de-compras.png?ex=67195608&is=67180488&hm=ab813ff91d3b68f16cbc81685e1b10b7851a6517a153e7209da2e0c7f475f551&"
+};
