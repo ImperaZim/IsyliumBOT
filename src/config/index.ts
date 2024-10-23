73273ebafb99b374caaf4a2942c1cf3f6393a9a0
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-22 21:31:23 -0300

    update src/config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,29 +1,31 @@
 import { Config } from "@types";
 
 export const discord: Config = {
-  id: "1261824986885390421",
-  token: "MTI2MTgyNDk4Njg4NTM5MDQyMQ.GI8k9s.097_qhDTDirViRxR1ElqByKj2O9xHHtPNkPJ68",
+    id: "1261824986885390421",
+    token: "MTI2MTgyNDk4Njg4NTM5MDQyMQ.GI8k9s.097_qhDTDirViRxR1ElqByKj2O9xHHtPNkPJ68"
 };
 export const mercadopago: Config = {
-  acesstoken: "APP_USR-6261249818160055-101222-b7a56ec6ebee67497829816254d9c498-779023770"
-}
+    acesstoken:
+        "APP_USR-6261249818160055-101222-b7a56ec6ebee67497829816254d9c498-779023770"
+};
 export const color: Config = {
-  red: "#f42121",
-  gray: "#2d2929",
-  azure: "#007af5",
-  light: "#0294ed",
-  midnight: "#18186a",
-  primary: "#ffdd00"
+    red: "#f42121",
+    gray: "#2d2929",
+    azure: "#007af5",
+    light: "#0294ed",
+    midnight: "#18186a",
+    primary: "#ffdd00"
 };
 
 export const profile: Config = {
-  icon: "https://cdn.discordapp.com/attachments/1261777733399347313/1295560563585454100/f1968b752dd03cce586e494eac75c237-1.png?ex=670f184c&is=670dc6cc&hm=c80a6d80e14b455d0fa9d49c63be64e9ea7ad278863a7ce2caf990d0e7d966b5&",
-  thumb: "https://cdn.discordapp.com/attachments/1261777733399347313/1295560563585454100/f1968b752dd03cce586e494eac75c237-1.png?ex=670f184c&is=670dc6cc&hm=c80a6d80e14b455d0fa9d49c63be64e9ea7ad278863a7ce2caf990d0e7d966b5&"
+    icon: "https://cdn.discordapp.com/attachments/1261777733399347313/1295560563585454100/f1968b752dd03cce586e494eac75c237-1.png?ex=670f184c&is=670dc6cc&hm=c80a6d80e14b455d0fa9d49c63be64e9ea7ad278863a7ce2caf990d0e7d966b5&",
+    thumb: "https://cdn.discordapp.com/attachments/1261777733399347313/1295560563585454100/f1968b752dd03cce586e494eac75c237-1.png?ex=670f184c&is=670dc6cc&hm=c80a6d80e14b455d0fa9d49c63be64e9ea7ad278863a7ce2caf990d0e7d966b5&"
 };
 
 export const assets: Config = {
-  ticket: "https://cdn.discordapp.com/attachments/1298374782810587219/1298374815127699477/4961736.png?ex=67195546&is=671803c6&hm=45b45964b3a4708ccf342017f6d793d153f909b9ec9dcc53972fd2dfa890f644&",
-  url: "https://cdn.discordapp.com/attachments/1298374782810587219/1298376388252012605/link.png?ex=671956bd&is=6718053d&hm=f88c913bf3ba1c00ce6a9e6f44ae323fad44410bd3040b0a957143b81f18c6dd&",
-  settings: "https://cdn.discordapp.com/attachments/1298374782810587219/1298427279679950848/settings_1.png?ex=67198623&is=671834a3&hm=f19fb551a61ffb3838afac720bd2c5f0d2d4d48bb1777407426832eaeda337a9&",
-  cart: "https://cdn.discordapp.com/attachments/1298374782810587219/1298375629636501545/carrinho-de-compras.png?ex=67195608&is=67180488&hm=ab813ff91d3b68f16cbc81685e1b10b7851a6517a153e7209da2e0c7f475f551&"
+    ticket: "https://cdn.discordapp.com/attachments/1298374782810587219/1298374815127699477/4961736.png?ex=67195546&is=671803c6&hm=45b45964b3a4708ccf342017f6d793d153f909b9ec9dcc53972fd2dfa890f644&",
+    dcl: "https://cdn.discordapp.com/attachments/1298374782810587219/1298433420313100329/connection.png?ex=67198bdb&is=67183a5b&hm=42516ebe32cba0d23d58fbd4aa5e6e6168a55649c53452dfeddf5bf87006cc82&",
+    settings:
+        "https://cdn.discordapp.com/attachments/1298374782810587219/1298427279679950848/settings_1.png?ex=67198623&is=671834a3&hm=f19fb551a61ffb3838afac720bd2c5f0d2d4d48bb1777407426832eaeda337a9&",
+    cart: "https://cdn.discordapp.com/attachments/1298374782810587219/1298375629636501545/carrinho-de-compras.png?ex=67195608&is=67180488&hm=ab813ff91d3b68f16cbc81685e1b10b7851a6517a153e7209da2e0c7f475f551&"
 };
