c53969417a812930cb7ca5e4ecaeff4558f3a85d
yBriisMC <87488283+yBriisMC@users.noreply.github.com>
2024-10-26 20:23:37 -0300

    update src/handlers/elementors/settings/builders/registerRows.ts

--- src/handlers/elementors/settings/builders/registerRows.ts
+++ src/handlers/elementors/settings/builders/registerRows.ts
@@ -10,44 +10,37 @@ export function registerRows(): void {
                 color: color.primary,
                 description:
                     "> ⏳ Estou carregando as configurações... Por favor, aguarde enquanto tudo é ajustado para você!",
-                thumbnail:
-                    "https://cdn.iconfinder.com/data/icons/general-icons-rounded/110/Settings-512.png"
             },
             settings_main_menu: {
                 color: color.primary,
                 description:
                     "> ⚙️ Olá, {user}! Bem-vindo ao menu de configurações do **Isylium Bot**! Aqui, você pode personalizar todas as funcionalidades do bot para o seu servidor.",
-                thumbnail:
-                    "https://cdn.iconfinder.com/data/icons/general-icons-rounded/110/Settings-512.png"
             },
             discord_link_settings: {
                 color: color.primary,
                 description:
                     "> 🔗 Olá, {user}! Configure a integração do bot com o Discord usando os botões abaixo. Consulte este [site](https://zira.bot/embedbuilder/) para criar JSONs de embeds, se precisar.",
-                thumbnail:
-                    "https://img.icons8.com/ios-filled/500/discord-integration.png"
             },
             discord_logs_setup: {
                 color: color.primary,
                 description:
-                    "> 📝 Selecione o canal para os **logs do Discord** para manter um histórico organizado das atividades no seu servidor.",
-                thumbnail:
-                    "https://cdn-icons-png.flaticon.com/512/3659/3659921.png"
+                    "> 📝 Selecione o canal para os **logs do Discord limk** para manter um histórico organizado das atividades no seu servidor.",
             },
             discord_send_setup: {
                 color: color.primary,
                 description:
                     "> 📢 Escolha o canal onde deseja enviar o painel do bot para o seu servidor.",
-                thumbnail:
-                    "https://img.icons8.com/ios-filled/500/discord-integration.png"
             },
             ticket_logs_setup: {
                 color: color.primary,
                 description:
                     "> 🎫 Escolha o canal para os **logs de tickets** do servidor para acompanhar as solicitações.",
-                thumbnail:
-                    "https://cdn-icons-png.flaticon.com/512/3050/3050293.png"
-            }
+            },
+            ticket_send_setup: {
+                color: color.primary,
+                description:
+                    "> 📢 Escolha o canal onde deseja enviar o painel de atendimento do bot para o seu servidor.",
+            },
         },
         components: {
             selects: {
@@ -87,7 +80,7 @@ export function registerRows(): void {
                     type: "channel",
                     data: {
                         disabled: false,
-                        placeholder: "Escolha o canal de logs",
+                        placeholder: "Escolha o canal para enviar",
                         minValue: 1,
                         maxValue: 1,
                         channelTypes: [ChannelType.GuildText]
@@ -102,46 +95,56 @@ export function registerRows(): void {
                         maxValue: 1,
                         channelTypes: [ChannelType.GuildText]
                     }
-                }
+                },
+                ticket_embed_select: {
+                    type: "channel",
+                    data: {
+                        disabled: false,
+                        placeholder: "Escolha o canal para enviar",
+                        minValue: 1,
+                        maxValue: 1,
+                        channelTypes: [ChannelType.GuildText]
+                    }
+                },
             },
             buttons: {
                 discord_embed_creator: {
-                    type: ButtonStyle.Secondary,
+                    type: ButtonStyle.Primary,
                     data: {
                         label: "Criador de Embed",
                         emoji: "✏️"
                     }
                 },
                 discord_log_channel: {
-                    type: ButtonStyle.Secondary,
+                    type: ButtonStyle.Primary,
                     data: {
                         label: "Canal de Logs",
                         emoji: "📋"
                     }
                 },
                 discord_server_manager: {
-                    type: ButtonStyle.Secondary,
+                    type: ButtonStyle.Primary,
                     data: {
                         label: "Gerenciar Servidor",
                         emoji: "🛠️"
                     }
                 },
                 ticket_embed_creator: {
-                    type: ButtonStyle.Secondary,
+                    type: ButtonStyle.Primary,
                     data: {
                         label: "Criador de Embed",
                         emoji: "✏️"
                     }
                 },
                 tickey_log_channel: {
-                    type: ButtonStyle.Secondary,
+                    type: ButtonStyle.Primary,
                     data: {
                         label: "Canal de Logs",
                         emoji: "📋"
                     }
                 },
                 ticket_role_perm: {
-                    type: ButtonStyle.Secondary,
+                    type: ButtonStyle.Primary,
                     data: {
                         label: "Gerenciar Permissões",
                         emoji: "🔒"
