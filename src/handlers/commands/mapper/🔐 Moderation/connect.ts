cb1c84e611a9cfda36deb18d9cbdeb9cc646e164
impera <imperaartz1@gmail.com>
2024-10-28 01:41:51 -0300

    ...

--- src/handlers/commands/mapper/🔐 Moderation/connect.ts
+++ src/handlers/commands/mapper/🔐 Moderation/connect.ts
@@ -1,22 +1,31 @@
 import { client } from "@main";
 import { CommandProps } from "@types";
 import { ExtendedCommand } from "@extensions";
-import { ApplicationCommandType } from "discord.js";
+import { ApplicationCommandType, ApplicationCommandOptionType } from "discord.js";
 
 export default new ExtendedCommand({
   name: "connect",
   description: "Link your account to any server.",
   defaultMemberPermissions: "Administrator",
+  options: [
+    {
+      name: "token",
+      description: "Envie um token, obtenha usando /discord no minecraft",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+  ],
   type: ApplicationCommandType.ChatInput,
-  async run({ interaction }: CommandProps) {
+  async run({ interaction, options }: CommandProps) {
     if (!interaction.inCachedGuild()) return;
 
     const { user, guild } = interaction;
+    const token = options.getUser("token");
 
     await interaction.reply({
       ephemeral: true,
       fetchReply: true,
-      content: 'comando em produção!'
+      content: 'comando em produção! TOKEN=' + token
     });
   }
 });
