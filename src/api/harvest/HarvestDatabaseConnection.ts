df0820cdb80340af46c72bae5300490d372970dd
impera <imperaartz1@gmail.com>
2024-10-28 03:36:15 -0300

    ...

--- src/api/harvest/HarvestDatabaseConnection.ts
+++ src/api/harvest/HarvestDatabaseConnection.ts
@@ -3,14 +3,14 @@ import { mysql } from "@main";
 export class HarvestDatabaseConnection {
 
   public static async setPlayerData(username: string, token: string): Promise<void> {
-    await await mysql.insert("harvest_players", {
+    await mysql.insert("harvest_players", {
       username: username,
       token: token
     });
   }
 
   public static async getUserToken(username: string): Promise<any> {
-    await await mysql.select(
+    await mysql.select(
       "harvest_players",
       "token",
       [{ username: username }]
